3d8fc1787d809d9d6c15184a0df6f5e929094e86
BingNa Yang <bingnayang@gmail.com>
2020-06-19 16:38:41 -0400

    Initial commit

---
+++ src/com/company/Quiz.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Quiz {
+    private int questionId;
+    private String quizQuestion;
+    private String questionAnswer;
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getQuizQuestion() {
+        return quizQuestion;
+    }
+
+    public void setQuizQuestion(String quizQuestion) {
+        this.quizQuestion = quizQuestion;
+    }
+
+    public String getQuestionAnswer() {
+        return questionAnswer;
+    }
+
+    public void setQuestionAnswer(String questionAnswer) {
+        this.questionAnswer = questionAnswer;
+    }
+}
