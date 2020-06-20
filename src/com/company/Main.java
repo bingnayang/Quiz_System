7dcb922a356b380784c7c71a6fc50a53cd1da188
BingNa Yang <bingnayang@gmail.com>
2020-06-20 12:41:55 -0400

    Reading XML file using DOM parser

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,70 @@
 package com.company;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String filePath = "/Users/Bing/Documents/GitHub/BingnaYang.github.io/Quiz_System/src/com/company/questions.xml";
+        File xmlFile = new File(filePath);
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder;
+
+        try{
+            documentBuilder = factory.newDocumentBuilder();
+            Document document = documentBuilder.parse(xmlFile);
+            System.out.println(document.getDocumentElement().getNodeName());
+            NodeList nodeList = document.getElementsByTagName("Quiz");
+
+            // Now XML is loaded as Document in memory
+            // Convert it to Object List
+            List<Quiz> quizList = new ArrayList<>();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                quizList.add(getQuestion(nodeList.item(i)));
+            }
+            // Print Quiz questions list information
+            for (Quiz question : quizList) {
+                System.out.println(question.toString());
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }catch (SAXException | ParserConfigurationException e1){
+            e1.printStackTrace();
+        }
     }
+
+    private static Quiz getQuestion(Node node) {
+        //XMLReaderDOM domReader = new XMLReaderDOM();
+        Quiz quizQuestion = new Quiz();
+        if (node.getNodeType() == Node.ELEMENT_NODE) {
+            Element element = (Element) node;
+            quizQuestion.setId(Integer.parseInt(getTagValue("id",element)));
+            quizQuestion.setQuestion(getTagValue("question",element));
+            quizQuestion.setOptionA(getTagValue("optionA",element));
+            quizQuestion.setOptionB(getTagValue("optionB",element));
+            quizQuestion.setOptionC(getTagValue("optionC",element));
+            quizQuestion.setOptionD(getTagValue("optionD",element));
+            quizQuestion.setAnswer(getTagValue("answer",element));
+        }
+        return quizQuestion;
+    }
+
+    private static String getTagValue(String tag, Element element) {
+        NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
+        Node node = (Node) nodeList.item(0);
+        return node.getNodeValue();
+    }
+
 }
--- src/com/company/Quiz.java
+++ src/com/company/Quiz.java
@@ -1,31 +1,78 @@
 package com.company;
 
 public class Quiz {
-    private int questionId;
-    private String quizQuestion;
-    private String questionAnswer;
+    private int id;
+    private String question;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String optionD;
+    private String answer;
 
-    public int getQuestionId() {
-        return questionId;
+    public int getId() {
+        return id;
     }
 
-    public void setQuestionId(int questionId) {
-        this.questionId = questionId;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public String getQuizQuestion() {
-        return quizQuestion;
+    public String getQuestion() {
+        return question;
     }
 
-    public void setQuizQuestion(String quizQuestion) {
-        this.quizQuestion = quizQuestion;
+    public void setQuestion(String question) {
+        this.question = question;
     }
 
-    public String getQuestionAnswer() {
-        return questionAnswer;
+    public String getOptionA() {
+        return optionA;
     }
 
-    public void setQuestionAnswer(String questionAnswer) {
-        this.questionAnswer = questionAnswer;
+    public void setOptionA(String optionA) {
+        this.optionA = optionA;
+    }
+
+    public String getOptionB() {
+        return optionB;
+    }
+
+    public void setOptionB(String optionB) {
+        this.optionB = optionB;
+    }
+
+    public String getOptionC() {
+        return optionC;
+    }
+
+    public void setOptionC(String optionC) {
+        this.optionC = optionC;
+    }
+
+    public String getOptionD() {
+        return optionD;
+    }
+
+    public void setOptionD(String optionD) {
+        this.optionD = optionD;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+    @Override
+    public String toString() {
+        return "=======================\n" +
+                "Question ID:" + this.id +
+                "\nQuestion: " + this.question +
+                "\nA) " + this.optionA +
+                "\nB) " + this.optionB +
+                "\nC) " + this.optionC +
+                "\nD) " + this.optionD +
+                "\nAnswer: " + this.answer;
     }
 }
